22077ee125483855e0013791d08a56801b9dc5a8
am406242 <am406242@382fa5d8-cbb6-401f-88f7-fd2afbc493df>
2019-10-31 15:17:39 +0000

    Fixed indents

--- poset.h
+++ poset.h
@@ -16,41 +16,23 @@ extern "C"
 
 #endif // __cplusplus
 
-    // a*
     unsigned long poset_new(void);
 
-    // a*
     void poset_delete(unsigned long id);
 
-    // a*
     size_t poset_size(unsigned long id);
 
-    // r
     bool poset_insert(unsigned long id, char const *value);
-    // check if name is unique
-    // add node
 
-    // a*
     bool poset_remove(unsigned long id, char const *value);
-    // remove edges
 
-    // a*
     bool poset_add(unsigned long id, char const *value1, char const *value2);
-    // check if new edge is ok
-    // add new edge and neighbours 
 
-    // r
     bool poset_del(unsigned long id, char const *value1, char const *value2);
-    // check if can delete edge
-    // delete edge
 
-    // r
     bool poset_test(unsigned long id, char const *value1, char const *value2);
-    // easy check
 
-    // r
     void poset_clear(unsigned long id);
-    // map clear
 
 #ifdef __cplusplus
 }
